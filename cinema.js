e332868045ff1fa3b865a18d7b578e5d0414ce57
JacquelineCasali <jcasalia@yahoo.com.br>
2022-05-28 12:27:33 -0300

    Criando função para listar todos os filmes

--- cinema.js
+++ cinema.js
@@ -3,14 +3,26 @@
 // JSON - ARRAY DE TRANSIÇÃO DE INFORMAÇÃO 
 //NOTAÇÃO DE OBJETO JAVASCRIPT
 //json converte en objeto
+//parte 1 do exercicio
+  var catalogo=require("./database/catalogo.json")
+// //console.log(catalogo)
+ var catalogostring=JSON.stringify(catalogo)//json stringfy converte objeto em string
+// //console.log(catalogostring)
+ var catalogoObj=JSON.parse(catalogostring)// json parse converte string em objeto
+// //console.log(catalogoObj)
 
-var catalogo=require("./database/catalogo.json")
-// console.log(catalogo)
-var catalogostring=JSON.stringify(catalogo)//json stringfy converte objeto em string
-console.log(catalogostring)
-var catalogoObj=JSON.parse(catalogostring)
-console.log(catalogoObj)
+//parte 2 do exercicio 
+// 1. listarTodosOsFilmes - criar 
+// A função deve percorrer toda a lista de ﬁlmes armazenada no catálogo utilizando o loop for e retornar as informações de maneira amigável ao usuário.
 
+function listarTodosOsFilmes(filmes) {
+  for (var i = 0; i < filmes.length; i++) {
+    console.log(filmes[i].titulo);
+      }
+}
+listarTodosOsFilmes(catalogoObj.data);
 
- console.log(catalogo)
-//console.log(catalogo.data)
+// 2. listarFilmesEmCartaz - criar
+// A função deve percorrer toda a lista de ﬁlmes armazenada no catálogo 
+// utilizando o loop for e retornar os ﬁlmes disponíveis em cartaz - as 
+// informações de maneira amigável ao usuário.
